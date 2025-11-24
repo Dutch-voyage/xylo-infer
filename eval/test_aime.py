81d1d505ca65e09fedeabe49c1ab5a9860248c23
Yang Yuxuan <yangyuxuanf1dt@gmail.com>
2025-11-24 07:23:53 +0000

    add eval script

--- eval/test_aime.py
+++ eval/test_aime.py
@@ -1,10 +1,12 @@
 import torch
 import numpy as np
-from src.services.nanovllm_v2 import LLM, SamplingParams
+from src.services.nanovllm_v5 import LLM, SamplingParams
 from transformers import AutoTokenizer
 import os
 import datasets
 from torch.utils.data import DataLoader, Dataset
+
+from .utils import evaluate
 
 
 temperature = 0.6
@@ -34,22 +36,63 @@ class Dataset_with_template(Dataset):
 def generate_answer(local_dir="datasets", model_path="/home/yyx/models/Qwen3-4B"):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     llm = LLM(model_path, enforce_eager=False, tensor_parallel_size=1)
-    sampling_params = SamplingParams(temperature=0.6 ,top_k=20, top_p=0.95, max_tokens=1024)
+    sampling_params = SamplingParams(temperature=0.6 ,top_k=20, top_p=0.95, max_tokens=24576)
+    # temperature < 0 for greedy_sampling
+    # sampling_params = SamplingParams(temperature=-1, max_tokens=1024)
     # model = AutoModelForCausalLM.from_pretrained(model_path).to("cuda")
     dataset = Dataset_with_template(local_dir, "aime_2024", tokenizer)
-    sample = dataset[5]
-    prompt = sample["raw_prompt"]
     
-    input_ids = tokenizer(prompt, return_tensors="pt")["input_ids"][0]
-    outputs = llm.generate([prompt], sampling_params)
+    batch_size = 30
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+    
+    scores = 0.0
+    generate_lengths = 0
+    length_list = []
+    logits_list = []
+    token_id_list = []
+    
+    for batch in dataloader:
+        
+        prompts = batch["raw_prompt"]
+        ground_truth = batch["ground_truth"]
+        
+        outputs = llm.generate(prompts, sampling_params)
 
-    output_ids = outputs[0]["token_ids"]
-    print(f"total output tokens {len(output_ids)}")
+        for idx in range(batch_size):
+            output_ids = outputs[idx]["token_ids"]
+            print(f"total output tokens {len(output_ids)}")
 
-    all_text = prompt + outputs[0]["text"]
-    # generated_text = outputs[0]["text"]
-    with open("aime_5_answer", "w") as f:
-        f.write(all_text)
+            length_list.append(len(output_ids))
+            logits_list.append(outputs[idx]["logits"])
+            token_id_list.append(output_ids)
+
+            score = evaluate(outputs[idx]["text"], ground_truth[idx])
+            scores += score
+            
+            generate_length = len(output_ids)
+            generate_lengths += generate_length
+
+            all_text = prompts[idx] + outputs[idx]["text"] + "\n\n" + "score: " + str(score) + "\n\n" + "generated_tokens: " + str(generate_length) + "\n\n" + "=" * 100 + "\n\n"
+            # generated_text = outputs[0]["text"]
+            with open("aime_baseline", "a") as f:
+                f.write(all_text)
+        
+    np.save("collected_data/test.npy", {"length_list": length_list, "logits_list": logits_list, "token_id_list": token_id_list})
+    
+    summary = "Evaluation completed." + "\n\n"
+    summary += "scores sum: " + str(scores) + "\n\n"
+    summary += f"Average score: {scores / batch_size}" + "\n\n"
+    summary += "Total generated tokens: " + str(generate_lengths) + "\n\n"
+    summary += f"Average generated tokens: {generate_lengths / batch_size}" + "\n\n"
+    with open("aime_baseline", "a") as f:
+        f.write(summary)
+    
+    print("Evaluation completed.")
+    print("scores sum:", scores)
+    print(f"Average score: {scores / batch_size}")
+    print("Total generated tokens:", generate_lengths)
+    print(f"Average generated tokens: {generate_lengths / batch_size}")
+    
     
 
 def main():
