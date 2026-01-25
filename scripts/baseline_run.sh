afcdde74d57e235a6ddd73b230ebd1b38bd977d7
Yang Yuxuan <yangyuxuanf1dt@gmail.com>
2026-01-25 07:17:12 +0000

    sync scripts

--- scripts/baseline_run.sh
+++ scripts/baseline_run.sh
@@ -1,10 +1,11 @@
-for p_attn in 0.60 0.70; do
+for p_attn in 0.60 0.70 0.80 0.90; do
   for layer_budget in 512 1024 2048; do
     for window_size in 32; do
       for steps_between_cache_compressions in 128; do
-
         python -m eval.test_aime_evict \
-            --compress_method snapkv \
+            --data_source aime25 \
+            --enforce_eager True \
+            --compress_method vanilla_topp \
             --if_fake_compress False \
             --layer_budget ${layer_budget} \
             --window_size ${window_size} \
--- scripts/run_compress_tokenwise.sh
+++ scripts/run_compress_tokenwise.sh
@@ -1,14 +1,29 @@
-for layer_budget in 1024 2048 4096; do
-  for window_size in 32; do
-    for steps_between_cache_compressions in 128; do
+# for layer_budget in 1024 2048 4096; do
+#   for data_source in aime24 aime25 umathtop50; do
+#     for window_size in 32; do
+#       for steps_between_cache_compressions in 128; do
+#         python -m eval.test_aime_baseline \
+#             --data_source ${data_source} \
+#             --enforce_eager True \
+#             --compress_method snapkv \
+#             --layer_budget ${layer_budget} \
+#             --window_size ${window_size} \
+#             --steps_between_cache_compressions ${steps_between_cache_compressions}
+#       done
+#     done
+#   done
+# done
 
-      python -m eval.test_aime_baseline \
-          --data_source umathtop50 \
-          --enforce_eager True \
-          --compress_method snapkv \
-          --layer_budget ${layer_budget} \
-          --window_size ${window_size} \
-          --steps_between_cache_compressions ${steps_between_cache_compressions}
-    done
+for layer_budget in 1024 2048 4096; do
+    for window_size in 32; do
+      for steps_between_cache_compressions in 128; do
+        python -m eval.test_aime_baseline \
+            --data_source aime24 \
+            --enforce_eager True \
+            --compress_method vanilla \
+            --layer_budget ${layer_budget} \
+            --window_size ${window_size} \
+            --steps_between_cache_compressions ${steps_between_cache_compressions}
+      done
   done
 done
